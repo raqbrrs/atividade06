bb6669bc74ac2db5be5777bc48a95a3ea312301d
raqbrrs <raquelbrrs07@gmail.com>
2024-09-09 08:36:09 -0300

    nome completo

---
+++ atividade06.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+nome = input("digite o primeiro nome  ")
+sobrenome = input ("digite o sobrenome")
+
+nome_completo = (nome  +sobrenome)
+print (nome_completo)
+
